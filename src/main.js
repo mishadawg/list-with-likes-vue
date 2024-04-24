a56ed3091b6cbdde90cabdfd2b31673e9118689d
misha_runner <misha40401@yandex.ru>
2024-04-24 14:33:56 +0300

    reset and add common styles.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+require("./assets/reset.css");
+require("./assets/main.css");
+
 createApp(App).use(router).mount("#app");
